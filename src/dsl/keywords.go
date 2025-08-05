f5e66e6285ed2aa6487feb30d7f99203fe1691ee
cenalon@yahoo.fr <cenalon@yahoo.fr>
2025-08-05 15:43:46 +0200

    Split symbols and keywords

---
+++ src/dsl/keywords.go
@@ -0,0 +1,11 @@
+package dsl
+
+// //////////////////////////////////
+// KEYWORDS FOR SOURCE MANAGEMENT //
+// //////////////////////////////////
+
+// module declaration
+const KW_MODULE = "topic"
+
+// import content from another module
+const KW_IMPORT = "import"
