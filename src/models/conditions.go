86dbdd44cef67961d55acf58166310836bfa5f48
cenalon@yahoo.fr <cenalon@yahoo.fr>
2025-09-29 09:15:05 +0200

    Improve conditions

--- src/models/conditions.go
+++ src/models/conditions.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 
@@ -19,8 +20,11 @@ type ValueOperator int
 // ValuesEqual test if values are the same (including case)
 const ValuesEqual ValueOperator = 1
 
-// ValuesEqualIgnoreCase test if values are the same no matter the case
+// ValuesEqualIgnoreCase tests if values are the same no matter the case
 const ValuesEqualIgnoreCase ValueOperator = 2
+
+// ValueMatchesRegexp tests if a value matches a given regexp as a reference
+const ValueMatchesRegexp ValueOperator = 3
 
 // valuesOperatorRun applies the ValueOperator on source (parameter) and reference (from condition)
 func valuesOperatorRun(source, reference string, operator ValueOperator) bool {
@@ -29,6 +33,12 @@ func valuesOperatorRun(source, reference string, operator ValueOperator) bool {
 		return source == reference
 	case ValuesEqualIgnoreCase:
 		return strings.EqualFold(source, reference)
+	case ValueMatchesRegexp:
+		if validator := regexp.MustCompile(reference); validator == nil {
+			return false
+		} else {
+			return validator.MatchString(source)
+		}
 	default:
 		return false
 	}
@@ -43,13 +53,18 @@ const NonDisjoinPeriods PeriodOperator = 1
 // SamePeriods tests if periods are equals
 const SamePeriods PeriodOperator = 2
 
-// AcceptsAllOperator returns true no matter the condition
-const AcceptsAllOperator PeriodOperator = 3
+// AcceptsAllPeriods returns true no matter the condition
+const AcceptsAllPeriods PeriodOperator = 3
+
+// IsIncludedInReferencePeriod tests if current period is in reference period
+const IsIncludedInReferencePeriod PeriodOperator = 4
 
 // periodOperatorRun returns true if operator applied to current and reference matches
 func periodOperatorRun(current, reference structures.Period, operator PeriodOperator) bool {
 	switch operator {
-	case AcceptsAllOperator:
+	case IsIncludedInReferencePeriod:
+		return reference.Intersection(current).Equals(reference)
+	case AcceptsAllPeriods:
 		return true
 	case NonDisjoinPeriods:
 		return !current.Intersection(reference).IsEmpty()
@@ -86,6 +101,28 @@ func (l LocalTypeCondition) Matches(e ModelEntity) bool {
 	}
 }
 
+// LocalActiveCondition means that a temporal entity is active during that reference period
+type LocalActiveCondition struct {
+	ReferencePeriod structures.Period
+}
+
+// Matches returns true if m is a temporal entity with a period included in reference period
+func (l LocalActiveCondition) Matches(m ModelEntity) bool {
+	if m == nil {
+		return false
+	} else if t, ok := m.(TemporalEntity); !ok {
+		return false
+	} else {
+		return periodOperatorRun(t.ActivePeriod(), l.ReferencePeriod, IsIncludedInReferencePeriod)
+	}
+}
+
+// NewActiveCondition builds a condition to be active during a provided period.
+// Condition is true if object has an active period that contains reference (or is equals to)
+func NewActiveCondition(reference structures.Period) LocalCondition {
+	return LocalActiveCondition{ReferencePeriod: reference}
+}
+
 // LocalMatchingAttributeCondition is a condition for an object attribute on a given period.
 // For instance, nationality = "french" during a period
 type LocalMatchingAttributeCondition struct {
@@ -93,7 +130,7 @@ type LocalMatchingAttributeCondition struct {
 	AttributeValue    string            // Value of the attribute to compare with
 	AttributeOperator ValueOperator     // Operator for value (such as equals)
 	ReferencePeriod   structures.Period // Period to match attribute during
-	PeriodOoperator   PeriodOperator    // Operator for the period (such as with at least a common point)
+	PeriodOperator    PeriodOperator    // Operator for the period (such as with at least a common point)
 }
 
 // Matches returns true if all of those conditions apply :
@@ -113,7 +150,7 @@ func (l LocalMatchingAttributeCondition) Matches(e ModelEntity) bool {
 	} else {
 		for value, period := range matchingValues {
 			if valuesOperatorRun(value, l.AttributeValue, l.AttributeOperator) {
-				if periodOperatorRun(period, l.ReferencePeriod, l.PeriodOoperator) {
+				if periodOperatorRun(period, l.ReferencePeriod, l.PeriodOperator) {
 					return true
 				}
 			}
@@ -121,4 +158,32 @@ func (l LocalMatchingAttributeCondition) Matches(e ModelEntity) bool {
 	}
 
 	return false
+}
+
+// NewAttributeValueCondition builds a time independant condition on attribute and value.
+// For instance, attribute = value no matter the time.
+func NewAttributeValueCondition(attribute, value string, operator ValueOperator) LocalCondition {
+	return LocalMatchingAttributeCondition{
+		AttributeName:     attribute,
+		AttributeValue:    value,
+		AttributeOperator: operator,
+		ReferencePeriod:   structures.NewFullPeriod(),
+		PeriodOperator:    AcceptsAllPeriods,
+	}
+}
+
+// NewAttributeRegexpCondition builds a condition for an attribute value matching a regexp no matter the period.
+// The regexp definition may not be valid, so we return an error if so.
+func NewAttributeRegexpCondition(attribute, regexpDefinition string) (LocalCondition, error) {
+	if _, err := regexp.Compile(regexpDefinition); err != nil {
+		return nil, err
+	}
+
+	return LocalMatchingAttributeCondition{
+		AttributeName:     attribute,
+		AttributeValue:    regexpDefinition,
+		AttributeOperator: ValueMatchesRegexp,
+		ReferencePeriod:   structures.NewFullPeriod(),
+		PeriodOperator:    AcceptsAllPeriods,
+	}, nil
 }
--- src/models_test/conditions_test.go
+++ src/models_test/conditions_test.go
@@ -29,7 +29,7 @@ func TestObjectAttributeCondition(t *testing.T) {
 		AttributeValue:    "M",
 		AttributeOperator: models.ValuesEqual,
 		ReferencePeriod:   structures.NewFullPeriod(),
-		PeriodOoperator:   models.AcceptsAllOperator,
+		PeriodOperator:    models.AcceptsAllPeriods,
 	}
 
 	objectNoMatch := models.NewObject([]string{"Human"})
@@ -58,7 +58,7 @@ func TestObjectAttributeCondition(t *testing.T) {
 		AttributeValue:    "F",
 		AttributeOperator: models.ValuesEqual,
 		ReferencePeriod:   structures.NewPeriodSince(date, true),
-		PeriodOoperator:   models.NonDisjoinPeriods,
+		PeriodOperator:    models.NonDisjoinPeriods,
 	}
 
 	mary := models.NewObjectSince([]string{"Human"}, birthdate)
@@ -66,5 +66,67 @@ func TestObjectAttributeCondition(t *testing.T) {
 	if !condition.Matches(mary) {
 		t.Fail()
 	}
+}
 
+func TestObjectAttributeRegexpCondition(t *testing.T) {
+	objectValueMatch := models.NewObject([]string{"Human"})
+	objectValueMatch.SetValue("attr", "abc")
+
+	if condition, err := models.NewAttributeRegexpCondition("attr", "\\w"); err != nil {
+		t.Log("valid regexp should not fail")
+		t.Fail()
+	} else if !condition.Matches(objectValueMatch) {
+		t.Log("regexp accepting any word should match attribute")
+		t.Fail()
+	}
+
+	objectValueMatch.SetValue("attr", "a")
+	if condition, err := models.NewAttributeRegexpCondition("attr", "\\d+"); err != nil {
+		t.Log("valid regexp should not fail")
+		t.Fail()
+	} else if condition.Matches(objectValueMatch) {
+		t.Log("regexp does not match value")
+		t.Fail()
+	}
+}
+
+func TestActiveConditionForTemporalEntities(t *testing.T) {
+	birthdate := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	now := time.Now().Truncate(time.Second)
+	after := now.AddDate(10, 0, 0)
+	valid := models.NewObjectSince([]string{"Human"}, birthdate)
+	condition := models.NewActiveCondition(structures.NewPeriodSince(now, true))
+	invalid := models.NewObjectSince([]string{"Human"}, after)
+
+	if !condition.Matches(valid) {
+		t.Fail()
+	} else if condition.Matches(invalid) {
+		t.Fail()
+	}
+
+	// and of course
+	object := models.NewObject([]string{"Human"})
+	if !condition.Matches(object) {
+		t.Fail()
+	}
+
+	// test forever condition
+	condition = models.NewActiveCondition(structures.NewFullPeriod())
+	if !condition.Matches(object) {
+		t.Fail()
+	}
+
+	// apply to link
+	other := models.NewObject([]string{"Human"})
+	likes, _ := models.NewTimedSimpleLink("likes", structures.NewPeriodSince(now, true), object, other)
+	if condition.Matches(likes) {
+		t.Log("likes cannot match because was false before now")
+		t.Fail()
+	}
+
+	// test when equals
+	condition = models.NewActiveCondition(structures.NewPeriodSince(now, true))
+	if !condition.Matches(likes) {
+		t.Fail()
+	}
 }
