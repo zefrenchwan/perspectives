83da68c51a8b0698e8a3477726ec0f3c37e95f21
zefrenchwan <cenalon@yahoo.fr>
2025-10-12 09:48:31 +0200

    cleaning parameters and getting rid of conditions

--- src/models/parameters.go
+++ src/models/parameters.go
@@ -4,37 +4,47 @@ package models
 // A condition does not depend on a single entity.
 // For instance, a join condition uses two entities.
 // So, to regroup all cases into a general form, we use Parameters.
-type Parameters interface {
+type Parameters[T ModelElement] interface {
 	// AppendAsVariable adds a new parameter as a variable
-	AppendAsVariable(name string, value ModelElement)
+	AppendAsVariable(name string, value T)
 	// Append adds an element at the end
-	Append(ModelElement)
+	Append(T)
 	// Size returns the number of positional elements for that parameter.
 	// It means the number of values ONLY
 	Size() int
 	// Get returns the positional argument for a given index.
 	// If there is no value for that index, return nil
-	Get(int) ModelElement
+	Get(int) T
 	// Variables returns the names of variables set
 	Variables() []string
 	// GetVariable returns the value for that variable, nil for no match
-	GetVariable(name string) ModelElement
+	GetVariable(name string) T
 	// IsEmpty returns true if parameters are empty and should be neglected
 	IsEmpty() bool
+	// SelectVariables picks variables by name to make a new parameter.
+	// Result contains no positional value, and variables if list if any
+	SelectVariables([]string) Parameters[T]
+	// Select picks values at given indexes to make a new parameter.
+	// Result contains only positional values, with selected indexes (if any)
+	Select([]int) Parameters[T]
+	// PositionalParameters returns the positional parameters as a slice
+	PositionalParameters() []T
+	// NamedParameters returns the named parameters as a map
+	NamedParameters() map[string]T
 }
 
 // genericParameters defines a basic implementation
 // as an array for positional elements
 // as a map for named elements
-type genericParameters struct {
+type genericParameters[T ModelElement] struct {
 	// positionals contain the positional arguments
-	positionals []ModelElement
+	positionals []T
 	// named contains named arguments
-	named map[string]ModelElement
+	named map[string]T
 }
 
 // Size returns the number of values in that parameter
-func (a *genericParameters) Size() int {
+func (a *genericParameters[T]) Size() int {
 	if a == nil {
 		return 0
 	}
@@ -43,22 +53,22 @@ func (a *genericParameters) Size() int {
 }
 
 // IsEmpty is true for no element
-func (a *genericParameters) IsEmpty() bool {
+func (a *genericParameters[T]) IsEmpty() bool {
 	return a == nil || (len(a.positionals) == 0 && len(a.named) == 0)
 }
 
 // Append adds a positional parameter
-func (a *genericParameters) Append(element ModelElement) {
+func (a *genericParameters[T]) Append(element T) {
 	if a != nil {
 		a.positionals = append(a.positionals, element)
 	}
 }
 
 // AppendAsVariable adds a new named value as a variable
-func (a *genericParameters) AppendAsVariable(name string, value ModelElement) {
+func (a *genericParameters[T]) AppendAsVariable(name string, value T) {
 	if a != nil {
 		if a.named == nil {
-			a.named = make(map[string]ModelElement)
+			a.named = make(map[string]T)
 		}
 
 		a.named[name] = value
@@ -66,16 +76,17 @@ func (a *genericParameters) AppendAsVariable(name string, value ModelElement) {
 }
 
 // Get returns the value at a given position, or nil if index does not match
-func (a *genericParameters) Get(index int) ModelElement {
-	if a == nil || index >= len(a.positionals) {
-		return nil
+func (a *genericParameters[T]) Get(index int) T {
+	var empty T
+	if a == nil || index < 0 || index >= len(a.positionals) {
+		return empty
 	}
 
 	return a.positionals[index]
 }
 
 // Variables returns the name of variables set for that parameter
-func (a *genericParameters) Variables() []string {
+func (a *genericParameters[T]) Variables() []string {
 	if a == nil {
 		return nil
 	}
@@ -89,25 +100,94 @@ func (a *genericParameters) Variables() []string {
 }
 
 // GetVariable returns the value (if any) for that name
-func (a *genericParameters) GetVariable(name string) ModelElement {
+func (a *genericParameters[T]) GetVariable(name string) T {
+	var empty T
 	if a == nil {
-		return nil
+		return empty
 	}
 
 	return a.named[name]
 }
 
+// SelectVariables reduces this parameters to only matching variables
+func (a *genericParameters[T]) SelectVariables(names []string) Parameters[T] {
+	if a == nil {
+		return nil
+	}
+
+	result := new(genericParameters[T])
+	if len(a.named) == 0 {
+		return result
+	}
+
+	result.named = make(map[string]T)
+	for _, name := range names {
+		if value, found := a.named[name]; found {
+			result.named[name] = value
+		}
+	}
+
+	return result
+}
+
+// Select reduces this parameters to only matching indexes
+func (a *genericParameters[T]) Select(indexes []int) Parameters[T] {
+	if a == nil {
+		return nil
+	}
+
+	result := new(genericParameters[T])
+	size := len(a.positionals)
+	if size == 0 {
+		return result
+	}
+
+	for _, index := range indexes {
+		if index >= 0 && index < size {
+			result.positionals = append(result.positionals, a.positionals[index])
+		}
+	}
+
+	return result
+}
+
+// PositionalParameters returns the positional parameters as a slice
+func (a *genericParameters[T]) PositionalParameters() []T {
+	var result []T
+	if a == nil {
+		return result
+	}
+
+	result = append(result, a.positionals...)
+
+	return result
+}
+
+// NamedParameters returns the named parameters as a map
+func (a *genericParameters[T]) NamedParameters() map[string]T {
+	if a == nil {
+		return nil
+	}
+
+	result := make(map[string]T)
+	for name, value := range a.named {
+		result[name] = value
+	}
+
+	return result
+}
+
 // NewParameter returns a new parameter for a single element
-func NewParameter(element ModelElement) Parameters {
-	result := new(genericParameters)
+func NewParameter[T ModelElement](element T) Parameters[T] {
+	result := new(genericParameters[T])
 	result.positionals = append(result.positionals, element)
 	return result
 }
 
 // NewNamedParameter returns a new parameter for a single named element
-func NewNamedParameter(name string, element ModelElement) Parameters {
-	result := new(genericParameters)
-	result.named = make(map[string]ModelElement)
+func NewNamedParameter[T ModelElement](name string, element T) Parameters[T] {
+	result := new(genericParameters[T])
+	result.named = make(map[string]T)
 	result.named[name] = element
 	return result
 }
---
+++ src/models_test/parameters_test.go
@@ -0,0 +1,139 @@
+package models_test
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/zefrenchwan/perspectives.git/models"
+)
+
+func TestParameterCreation(t *testing.T) {
+	durian := models.NewObject([]string{"Fruit"})
+	p := models.NewNamedParameter("x", durian)
+
+	if p.IsEmpty() {
+		t.Fail()
+	}
+
+	empty := p.Select([]int{})
+	if !empty.IsEmpty() {
+		t.Fail()
+	}
+}
+
+func TestParametersGet(t *testing.T) {
+	tanguy := models.NewObject([]string{"Human"})
+	alan := models.NewObject([]string{"Human"})
+
+	p := models.NewParameter(tanguy)
+	p.Append(alan)
+
+	if p.Get(0) != tanguy {
+		t.Fail()
+	} else if p.Get(1) != alan {
+		t.Fail()
+	} else if p.Get(-1) != nil {
+		t.Fail()
+	}
+
+	if value := p.PositionalParameters(); len(value) != 2 {
+		t.Fail()
+	} else if !slices.Contains(value, tanguy) {
+		t.Fail()
+	} else if !slices.Contains(value, alan) {
+		t.Fail()
+	} else if named := p.NamedParameters(); len(named) != 0 {
+		t.Fail()
+	}
+
+	q := models.NewNamedParameter("x", tanguy)
+	q.AppendAsVariable("y", alan)
+
+	variables := q.Variables()
+	slices.Sort(variables)
+	if slices.Compare(variables, []string{"x", "y"}) != 0 {
+		t.Fail()
+	}
+
+	if q.GetVariable("x") != tanguy {
+		t.Fail()
+	} else if q.GetVariable("y") != alan {
+		t.Fail()
+	} else if q.GetVariable("z") != nil {
+		t.Fail()
+	} else if q.Get(0) != nil {
+		t.Fail()
+	}
+
+	if values := q.NamedParameters(); len(values) != 2 {
+		t.Fail()
+	} else if v := values["x"]; v != tanguy {
+		t.Fail()
+	} else if v := values["y"]; v != alan {
+		t.Fail()
+	} else if len(q.PositionalParameters()) != 0 {
+		t.Fail()
+	}
+
+	r := models.NewParameter(tanguy)
+	r.AppendAsVariable("x", alan)
+
+	if r.Get(0) != tanguy {
+		t.Fail()
+	} else if r.Size() != 1 {
+		t.Fail()
+	} else if slices.Compare([]string{"x"}, r.Variables()) != 0 {
+		t.Fail()
+	} else if names := r.NamedParameters(); len(names) != 1 {
+		t.Fail()
+	} else if names["x"] != alan {
+		t.Fail()
+	} else if positionals := r.PositionalParameters(); len(positionals) != 1 {
+		t.Fail()
+	} else if positionals[0] != tanguy {
+		t.Fail()
+	}
+}
+
+func TestParameterSelect(t *testing.T) {
+	camembert := models.NewObject([]string{"Cheese"})
+	brie := models.NewObject([]string{"Cheese"})
+
+	variable := models.NewNamedParameter("x", &brie)
+	variable.AppendAsVariable("y", &camembert)
+
+	// test empty
+	result := variable.Select([]int{0, 1})
+	if !result.IsEmpty() {
+		t.Fail()
+	}
+
+	result = variable.SelectVariables([]string{"a", "b"})
+	if !result.IsEmpty() {
+		t.Fail()
+	}
+
+	// test select
+	result = variable.SelectVariables([]string{"x"})
+	if value := result.GetVariable("x"); value != &brie {
+		t.Fail()
+	}
+
+	// test select ints
+	other := models.NewParameter(&camembert)
+	other.Append(&brie)
+
+	if !other.Select(nil).IsEmpty() {
+		t.Fail()
+	} else if !other.Select([]int{-1, 2, 3, 4}).IsEmpty() {
+		t.Fail()
+	}
+
+	reduced := other.Select([]int{0})
+	if reduced.Size() != 1 {
+		t.Fail()
+	} else if reduced.Get(0) != &camembert {
+		t.Fail()
+	}
+
+}
