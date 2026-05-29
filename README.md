1ef58f16f0c00cb80ca247912e8a51b9024962d0
cenalon@yahoo.fr <cenalon@yahoo.fr>
2026-05-29 19:04:35 +0200

    new version for operands

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ It registers incoming information and build a state for information over time.
 To manage real world information, this project uses concepts such as traits to regroup common behavior, objects, and links. 
 Traits represent concepts, objects are instances of traits, and links represent relationships between them.
 Links and instances are valid during a given period of time. 
-A formal class system defines what is an element (to find if current element is a trait, or an object, or a link). 
+A formal class system defines what is an element (to determine whether current element is a trait, or an object, or a link). 
 
 
 For instance, Paul, Marie and John are objects of the trait PERSON, and they are linked by the link FRIEND_OF.
--- src/commons/graphs.go
+++ src/commons/graphs.go
@@ -12,19 +12,69 @@ type Linkable interface {
 	Element
 }
 
+// linkOperand represents a collection of linkable values that can be sorted or not.
+type linkOperand struct {
+	values []Linkable // values as the raw values, duplicates are possible
+	sorted bool       // sorted is true for lists, false for sets
+}
+
+// addValue adds a new value to the operand collection, updating the sorted flag if necessary.
+// It does nothing if value is nil, not even changing the sorted value.
+func (op *linkOperand) addValue(value Linkable, sorted bool) {
+	if value == nil {
+		return
+	}
+
+	op.values = append(op.values, value)
+	op.sorted = sorted
+}
+
+// copyValues returns a copy of the operand values (not values, just the slice)
+func (op *linkOperand) copyValues() []Linkable {
+	return slices.Clone(op.values)
+}
+
+// remove removes all values from the operand collection that satisfy the given predicate.
+func (op *linkOperand) remove(predicate func(Linkable) bool) {
+	result := make([]Linkable, 0)
+	for _, value := range op.values {
+		if !predicate(value) {
+			result = append(result, value)
+		}
+	}
+	op.values = result
+}
+
+// size returns the number of values in the operand collection.
+func (op *linkOperand) size() int {
+	if op == nil {
+		return 0
+	}
+
+	return len(op.values)
+}
+
+// newLinkOperand creates a new link operand with an empty slice and sorted set to false.
+func newLinkOperand() *linkOperand {
+	return &linkOperand{
+		values: make([]Linkable, 0),
+		sorted: false,
+	}
+}
+
 // Link represents a link between nodes in a graph.
 type Link struct {
-	locks    sync.RWMutex          // locks manage the locks for goroutines
-	name     string                // name of the link
-	operands map[string][]Linkable // operands of the link
-	validity Period                // moments the link is true
+	locks    sync.RWMutex            // locks manage the locks for goroutines
+	name     string                  // name of the link
+	operands map[string]*linkOperand // operands of the link
+	validity Period                  // moments the link is true
 }
 
 // NewLink creates a new empty link with provided name.
 func NewLink(name string) *Link {
 	return &Link{
 		name:     name,
-		operands: make(map[string][]Linkable),
+		operands: make(map[string]*linkOperand),
 		validity: NewFullPeriod(),
 	}
 }
@@ -48,8 +98,29 @@ func (l *Link) Same(other Element) bool {
 		return false
 	}
 
-	// TODO : implement it
+	otherLink, ok := other.(*Link)
+	if !ok {
+		return false
+	}
+	// at this point, both are links.
+
+	l.locks.RLock()
+	otherLink.locks.RLock()
+	defer l.locks.RUnlock()
+	defer otherLink.locks.RUnlock()
+
+	current := l
+	matchingCurrent := otherLink
+
+	if l.name != otherLink.name {
+		return false
+	} else if len(current.operands) != len(matchingCurrent.operands) {
+		return false
+	}
+
+	// todo : end it
 	return true
+
 }
 
 // DeclaringClass returns the class that declares the link, obviously including CLASS_LINK itself
@@ -108,13 +179,25 @@ func (l *Link) Has(name string) ([]Linkable, bool) {
 	defer l.locks.RUnlock()
 
 	operands, ok := l.operands[name]
-	return SliceCopy(operands), ok
+	return operands.copyValues(), ok
 }
 
-// Add adds a new operand to the link with the given name.
+// Add adds a new operand to the link with the given name if order does not matter.
 // For instance, Likes(subject=[Jean],Object=[Pizza]).Add("Object", "Tiramisu")
 // will make Likes(subject=[Jean],Object=[Pizza, Tiramisu])
 func (l *Link) Add(name string, operand Linkable) {
+	l.addOperand(name, operand, true)
+}
+
+// Append adds a new operand to the link with the given name if order DOES matter.
+// For instance, presidents of a country in order
+func (l *Link) Append(name string, operand Linkable) {
+	l.addOperand(name, operand, false)
+}
+
+// addOperand adds a new operand to the link with the given name and sorting preference.
+// if value is nil, it does nothing, not even changing the sorted value.
+func (l *Link) addOperand(name string, operand Linkable, sorted bool) {
 	if l == nil || operand == nil {
 		return
 	}
@@ -122,10 +205,18 @@ func (l *Link) Add(name string, operand Linkable) {
 	l.locks.Lock()
 	defer l.locks.Unlock()
 
-	l.operands[name] = append(l.operands[name], operand)
+	if previous, ok := l.operands[name]; ok && previous != nil {
+		previous.addValue(operand, sorted)
+		l.operands[name] = previous
+	} else {
+		current := newLinkOperand()
+		current.addValue(operand, sorted)
+		l.operands[name] = current
+	}
 }
 
 // Remove for a given role, specific values by predicate.
+// If the predicate matches all values, the role is removed from the link.
 func (l *Link) Remove(name string, op func(linkable Linkable) bool) {
 	if l == nil || op == nil {
 		return
@@ -134,13 +225,14 @@ func (l *Link) Remove(name string, op func(linkable Linkable) bool) {
 	l.locks.Lock()
 	defer l.locks.Unlock()
 
-	newValues := make([]Linkable, 0)
-	for _, element := range l.operands[name] {
-		if !op(element) {
-			newValues = append(newValues, element)
+	if operand, ok := l.operands[name]; ok {
+		operand.remove(op)
+		if operand.size() != 0 {
+			l.operands[name] = operand
+		} else {
+			delete(l.operands, name)
 		}
 	}
-	l.operands[name] = newValues
 }
 
 // Operands return a copy of the role map
@@ -154,7 +246,7 @@ func (l *Link) Operands() map[string][]Linkable {
 
 	result := make(map[string][]Linkable)
 	for k, v := range l.operands {
-		result[k] = SliceCopy(v)
+		result[k] = v.copyValues()
 	}
 
 	return result
