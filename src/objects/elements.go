e825ffe05e3a12cec177ce958773f2a17970b4f7
cenalon@yahoo.fr <cenalon@yahoo.fr>
2026-06-11 22:53:25 +0200

    introducing observables

--- src/objects/elements.go
+++ src/objects/elements.go
@@ -1,16 +1,23 @@
 package objects
 
+import "time"
+
 // Element is a system entity : for instance, traits, instances, links, etc.
 type Element interface {
-	Same(other Element) bool // Same checks if two elements are functionally equivalent
-	DeclaringClass() Class   // DeclaringClass returns the class for that element.
+	// Same checks if two elements are functionally equivalent
+	Same(other Element) bool
+	// DeclaringClass returns the class for that element.
+	DeclaringClass() Class
 }
 
-// IdentifiableElement is an element that may be distinguished by an identifier.
+// Observable is an element that may be distinguished by an identifier and we may observe at a given time.
 // It is unique, meaning that one may decide whether two elements are the same based on their identifier.
-// When implementing IdentifiableElement, ensure that the Same function is consistent with the identifier.
+// When implementing Observable, ensure that the Same function is consistent with the identifier.
 // Typical implementations should then be same if id and classes match
-type IdentifiableElement interface {
-	Element     // Element to ensure that classes logic applies
-	Id() string // Id returns the unique identifier for the element
+type Observable[T any] interface {
+	Element // Element to ensure that classes logic applies
+	// Id returns the unique identifier for the element
+	Id() string
+	// Observe returns the value of the observable at a given time
+	Observe(time.Time) T
 }
--- src/objects/instances.go
+++ src/objects/instances.go
@@ -1,14 +1,11 @@
 package objects
 
-import "time"
-
 // Instance is an element that may change over time, but we may identify as unique even for a state change.
 // We use a BItemporal model, so it means that there are two time dimensions:
 // the time of the instance itself (what changed during its lifetime) and the time of the observation.
 type Instance interface {
-	// IdentifiableElement to provide a unique identifier for the instance.
-	IdentifiableElement
-	// Observe returns the state of the instance at the given time.
-	// State itself contains the full content over time as far as we know at that obervation time. 
-	Observe(time.Time) TimeDependentContent
+	// Observable to provide
+	// a unique identifier for the instance (distinguishing it from other instances)
+	// an observation as a TimeDependentContent (historical data)
+	Observable[TimeDependentContent]
 }
