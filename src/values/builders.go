aabb111df593183d6be752abf052ef088a5a6799
cenalon@yahoo.fr <cenalon@yahoo.fr>
2026-07-19 11:53:37 +0200

    improve comments on builder and mapping

--- src/values/builders.go
+++ src/values/builders.go
@@ -2,15 +2,28 @@ package values
 
 import "github.com/zefrenchwan/perspectives.git/periods"
 
+// ReferenceMappingBuilder is a toolbox to build a mapping of periods to reference values.
 type ReferenceMappingBuilder interface {
+	// Add adds a reference value to the mapping for the given period.
+	// It may raise an error, such as when the reference is empty
 	Add(reference string, period periods.Period) error
+	// Remove removes a reference value from the mapping for the given period.
 	Remove(periods.Period)
+	// Build builds a mapping of periods to reference values.
+	// It may raise an error, if values are inconsistent.
 	Build() (ImmutableValuesMapping[ReferenceValue], error)
 }
 
+// PrimitiveMappingBuilder is a toolbox to build a mapping of periods to primitive values.
+// It decorates a mapping of periods to primitive values to allow access to core values (not primitive decorator).
 type PrimitiveMappingBuilder interface {
+	// ValuesType returns the type of values that this builder can build.
 	ValuesType() string
+	// Add adds raw values to the mapping for the given period.
 	Add(value any, period periods.Period) error
+	// Remove removes a raw value from the mapping for the given period.
 	Remove(periods.Period)
+	// Build builds a mapping of periods to primitive values.
+	// It may raise an error, if values types are inconsistent.
 	Build() (ImmutableValuesMapping[PrimitiveValue], error)
 }
--- src/values/mappings.go
+++ src/values/mappings.go
@@ -7,6 +7,8 @@ import (
 	"github.com/zefrenchwan/perspectives.git/periods"
 )
 
+// EnsureValuesMappingInvariant goes through a mapping and ensures that all values have the same type.
+// It returns true if all values have the same expected type and false otherwise.
 func EnsureValuesMappingInvariant[V Value](rawMapping periods.DynamicMapping[V]) bool {
 	var expectedType string
 	for _, value := range rawMapping.Range() {
@@ -26,9 +28,17 @@ func EnsureValuesMappingInvariant[V Value](rawMapping periods.DynamicMapping[V])
 	return expectedType == rawMapping.DataType()
 }
 
+// ImmutableValuesMapping is an immutable mapping of periods to values (reference or primitive values).
+// It is used to represent a mapping of periods to values that cannot be modified after creation.
 type ImmutableValuesMapping[V Value] interface {
+	// Hashable is an interface that provides a hash function for the mapping.
+	// Because mapping is immutable, it will not change after creation.
 	commons.Hashable
+	// IsEmpty returns true if the mapping is empty and false otherwise.
 	IsEmpty() bool
+	// Range returns an iterator over periods and values in the mapping.
 	Range() iter.Seq2[periods.Period, V]
+	// ValuesType returns the type of values in the mapping.
+	// For instance, on a primitive mapping, it will return "int", "string", etc.
 	ValuesType() string
 }
